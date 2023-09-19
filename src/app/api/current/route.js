b34762f3bb67d826e0f49358532c2645bb57a5aa
abhidcoder <74507289+abhidcoder@users.noreply.github.com>
2023-09-19 21:24:19 +0530

    Update_route.js

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -1,12 +1,26 @@
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 import { User } from "@/models/user";
-export async function GET(request) {
-  const authToken = request.cookies.get("authToken")?.value;
-  console.log(authToken);
-  const data = jwt.verify(authToken, process.env.JWT_KEY);
-  console.log(data);
-  const user = await User.findById(data._id).select("-password");
-  //   console.log("test");
-  return NextResponse.json(user);
+
+export async function POST(request) {
+  try {
+    const { email, password } = request.body;
+
+    // Perform validation on email and password here if needed
+
+    // Create a new user
+    const newUser = new User({ email, password });
+    await newUser.save();
+
+    // Generate an authentication token for the newly created user
+    const authToken = jwt.sign({ _id: newUser._id }, process.env.JWT_KEY);
+
+    // Set the authToken as a cookie or response header, depending on your authentication strategy
+
+    return NextResponse.json({ message: "Signup successful", authToken });
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.error("Internal Server Error", { status: 500 });
+  }
 }
+
